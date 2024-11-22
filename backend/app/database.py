a6e4b0f81f2eba67c5af5c88208772bd52d478ad
JeffTheNinja57 <mihny.angheluta@gmail.com>
2024-11-22 19:32:47 +0100

    change database dependency...again

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,15 @@
 import os
 from sqlmodel import SQLModel, create_engine, Session
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///backend/data/animals.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data/animals.db")
 
 engine = create_engine(DATABASE_URL, echo=True)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
